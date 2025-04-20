638f19ef7448523c0def7cb4117c9db4184d3308
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-20 16:57:14 -0500

    finished payrollcalulator

---
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class PayrollCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = "";
+        scanner.nextLine();
+
+        System.out.println("Enter hours worked: ");
+        double hoursWorked = scanner.nextDouble();
+
+        System.out.println("Enter your pay rate: ");
+        double payRate = scanner.nextDouble();
+
+        double total = hoursWorked * payRate;
+        System.out.println("Your gross pay is: " + total);
+
+
+
+        scanner.close();
+
+
+
+    }
+}
